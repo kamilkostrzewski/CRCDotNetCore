78c729018c1c4b9f0aa581d3593393782af4c559
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-03 17:09:01 +0200

    Add measurement repository

---
+++ src/DotNetCoreWebAPI/Repository/MeasurementRepository.cs
@@ -0,0 +1,50 @@
+﻿using DotNetCoreWebApi.DbContexts;
+using DotNetCoreWebApi.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotNetCoreWebAPI.Repository
+{
+    public class MeasurementRepository : IMeasurementRepository<Measurement>
+    {
+        private readonly MeasurementContext _dbContext;
+
+        public MeasurementRepository(MeasurementContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Measurement>> GetAll()
+        {
+            return await _dbContext.Measurements.ToListAsync();
+        }
+
+        public async Task<Measurement> Get(long id)
+        {
+            return await _dbContext.Measurements.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task Add(Measurement entity)
+        {
+            await _dbContext.Measurements.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Update(Measurement measurement, Measurement entity)
+        {
+            measurement.Name = entity.Name;
+            measurement.Value = entity.Value;
+            measurement.CreatedBy = entity.CreatedBy;
+            measurement.CreatedAt = entity.CreatedAt;
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(Measurement measurement)
+        {
+            _dbContext.Measurements.Remove(measurement);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
