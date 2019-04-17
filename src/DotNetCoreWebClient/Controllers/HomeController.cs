438fda4c57e6b0973f1d1764a7a3909d51db0a16
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-17 17:08:11 +0200

    Add blank measurements site

--- src/DotNetCoreWebClient/Controllers/HomeController.cs
+++ src/DotNetCoreWebClient/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace DotNetCoreWebClient.Controllers
             return View();
         }
 
+        public IActionResult Measurements()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
