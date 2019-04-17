8125449c97fd04c23da5f3ba4581e0897d7b4ebe
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-17 17:52:50 +0200

    Add controller for measurement section

---
+++ src/DotNetCoreWebClient/wwwroot/js/new-measurement-section.js
@@ -0,0 +1,27 @@
+﻿class NewMeasurementSection {
+    _listeners = [];
+
+    constructor() {
+        let addBtn = document.querySelector('#measurements_addBtn');
+        addBtn.addEventListener('click', e => {
+            let nameInput = document.querySelector('#measurements_nameInput');
+            let valueInput = document.querySelector('#measurements_valueInput');
+            let obj = {
+                name: nameInput.value,
+                value: valueInput.value
+            };
+
+            this._raiseNewMeasurementAdded(obj);
+        })
+    }
+
+    addEventListener(listener) {
+        this._listeners.push(listener);
+    }
+
+    _raiseNewMeasurementAdded(e) {
+        this._listeners.forEach(l => {
+            l.newMeasurementAdded(e);
+        })
+    }
+}
