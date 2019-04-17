c15c548ec6b765919cad2189ca11ed9d5dc9185f
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-17 18:00:21 +0200

    Remove semicolons

--- src/DotNetCoreWebClient/wwwroot/js/new-measurement-section.js
+++ src/DotNetCoreWebClient/wwwroot/js/new-measurement-section.js
@@ -1,27 +1,27 @@
 ﻿class NewMeasurementSection {
-    _listeners = [];
+    _listeners = []
 
     constructor() {
-        let addBtn = document.querySelector('#measurements_addBtn');
+        let addBtn = document.querySelector('#measurements_addBtn')
         addBtn.addEventListener('click', e => {
-            let nameInput = document.querySelector('#measurements_nameInput');
-            let valueInput = document.querySelector('#measurements_valueInput');
+            let nameInput = document.querySelector('#measurements_nameInput')
+            let valueInput = document.querySelector('#measurements_valueInput')
             let obj = {
                 name: nameInput.value,
                 value: valueInput.value
-            };
+            }
 
-            this._raiseNewMeasurementAdded(obj);
+            this._raiseNewMeasurementAdded(obj)
         })
     }
 
     addEventListener(listener) {
-        this._listeners.push(listener);
+        this._listeners.push(listener)
     }
 
     _raiseNewMeasurementAdded(e) {
         this._listeners.forEach(l => {
-            l.newMeasurementAdded(e);
+            l.newMeasurementAdded(e)
         })
     }
 }
