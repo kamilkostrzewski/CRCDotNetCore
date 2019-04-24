85ba483997c4a282f22fc0cd3e5a7e3e98be95e0
Kamil Kostrzewski <kamil.kostrzewski95@gmail.com>
2019-04-24 17:15:37 +0200

    Add measurement-controller

---
+++ src/DotNetCoreWebClient/wwwroot/js/measurement-controller.js
@@ -0,0 +1,12 @@
+﻿class MeasurementController {
+    constructor() {
+        this._newMeasurementSection = new NewMeasurementSection()
+        this._newMeasurementSection.addEventListener(new class {
+            newMeasurementAdded(e) {
+                debugger
+            }
+        })
+    }
+}
+
+(() => new MeasurementController())()
